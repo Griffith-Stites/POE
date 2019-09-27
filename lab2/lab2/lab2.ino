0d308ccd4083cc78dd70c8ac514ffb6136fdbad7
Griffith-Stites <gjstites@gmail.com>
2019-09-27 09:28:33 -0400

    Update comments on arduino code

--- lab2/lab2/lab2.ino
+++ lab2/lab2/lab2.ino
@@ -1,11 +1,11 @@
 #include <Servo.h> // Include the servo library
 
 // These constants won't change. They're used to give names to the pins used:
-const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
-const int panPin = 5; // Digital input pin for the servo angle signal
-const int tiltPin = 6; // Digital input pin for the servo angle signal
+const int analogInPin = A0;  // Analog input pin that the IR sensor
+const int panPin = 5; // Digital output pin for the servo angle signal
+const int tiltPin = 6; // Digital output pin for the servo angle signal
 
-int sensorValue = 0;        // value read from the pot
+int sensorValue = 0; // value read from the IR sensor
 int pan = 0; // variable to store the servo position
 
 Servo servo1;  // create servo object to control a servo
@@ -13,13 +13,13 @@ Servo servo1;  // create servo object to control a servo
 void setup() {
   // initialize serial communications at 9600 bps:
   Serial.begin(9600);
-  servo1.attach(panPin);  // attaches the servo on pin 9 to the servo object
+  servo1.attach(panPin);  // attaches the servo on pin 5 to the servo object
 }
 
 void loop() {
   for (pan = 0; pan <= 60; pan += 1) { // goes from 0 degrees to 60 degrees in steps of 1 degree
     servo1.write(pan + 60); // tell servo1 to go to position in variable 'pan'
-    delay(15); // waits 15ms for the servo to reach the position
+//    delay(15); // waits 15ms for the servo to reach the position
     delay(100); // waits 100ms for the sensor power to fluctuate, 40ms according to the datasheet, 100ms for good measure
     sensorValue = analogRead(analogInPin); // read the sensnor value
     printStatus(sensorValue, pan, 1);
