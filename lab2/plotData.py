20fda18e7e33396cbde8ff0d54aecbe1988c716c
Jerry Goss <jerry.h.goss@gmail.com>
2019-09-29 13:15:30 -0400

    plotData.py

--- lab2/plotData.py
+++ lab2/plotData.py
@@ -1,1 +1,28 @@
-import matplotlib
+import matplotlib.pyplot as plt
+# import numpy as np
+# import pandas as pd
+import pickle
+
+objects = []
+with open('data.txt', 'rb') as openfile:
+    while True:
+        try:
+            objects.append(pickle.load(openfile))
+        except EOFError:
+            break
+
+objects = objects[0]
+# print(objects[0][1])
+
+x = []
+y = []
+
+for i in range(len(objects)-1):
+    x.append(objects[i][0])
+    y.append(objects[i][1])
+
+plt.scatter(x, y, c='b', alpha=0.5)
+plt.title('2D ScatterPlot of our Letter')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
