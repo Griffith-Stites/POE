0dbdb3c8ff9411793b6273b81c47312af7ed2f53
Griffith-Stites <gjstites@gmail.com>
2019-09-29 16:23:03 -0400

    Add averaging capabilities

--- lab2/arduino2D/arduino2D.ino
+++ lab2/arduino2D/arduino2D.ino
@@ -5,7 +5,12 @@ const int analogInPin = A0;  // Analog input pin that the IR sensor
 const int panPin = 5; // Digital output pin for the servo angle signal
 const int tiltPin = 6; // Digital output pin for the servo angle signal
 
-int sensorValue = 0; // value read from the IR sensor
+// values read from the IR sensor
+int sensorValue = 0;
+int read1 = 0;
+int read2 = 0;
+int read3 = 0;
+
 int pan = 0; // variable to store the servo position
 
 Servo servo1;  // create servo object to control a servo
@@ -18,17 +23,27 @@ void setup() {
 
 void loop() {
   Serial.println("start");
-  for (pan = 0; pan <= 60; pan += 1) { // goes from 0 degrees to 60 degrees in steps of 1 degree
-    servo1.write(pan + 60); // tell servo1 to go to position in variable 'pan'
-    //    delay(15); // waits 15ms for the servo to reach the position
+  for (pan = 80; pan <= 110; pan += 1) { // goes from 0 degrees to 60 degrees in steps of 1 degree
+    servo1.write(pan); // tell servo1 to go to position in variable 'pan'
     delay(100); // waits 100ms for the sensor power to fluctuate, 40ms according to the datasheet, 100ms for good measure
-    sensorValue = analogRead(analogInPin); // read the sensnor value
+    sensorValue = readSensor();
     printStatus(sensorValue, pan, 1);
   }
   Serial.println("finished");
   delay(3000);
 }
 
+int readSensor(){
+  // read the sensor and average the results
+  read1 = analogRead(analogInPin); // read the sensor value
+  delay(15);
+  read2 = analogRead(analogInPin); // read the sensor value
+  delay(15);
+  read3 = analogRead(analogInPin); // read the sensor value
+  delay(15);
+  sensorValue = (read1 + read2 + read3) / 3;
+  return sensorValue;
+}
 
 void printStatus(int sensor, int pan, int tilt) {
   // print the results to the Serial Monitor:
