fd034b35593cb16415949e509bb86963611b9165
Griffith-Stites <gjstites@gmail.com>
2019-09-29 16:22:48 -0400

    Add 3d code

--- lab2/arduino3D/arduino3D.ino
+++ lab2/arduino3D/arduino3D.ino
@@ -5,30 +5,54 @@ const int analogInPin = A0;  // Analog input pin that the IR sensor
 const int panPin = 5; // Digital output pin for the servo angle signal
 const int tiltPin = 6; // Digital output pin for the servo angle signal
 
-int sensorValue = 0; // value read from the IR sensor
-int pan = 0; // variable to store the servo position
+// values read from the IR sensor
+int sensorValue = 0;
+int read1 = 0;
+int read2 = 0;
+int read3 = 0;
 
-Servo servo1;  // create servo object to control a servo
+int pan = 0; // variable to store the pan
+int tilt = 0; // variable to store the tilt
+
+Servo servoPan;  // create servo object to control pan servo
+Servo servoTilt; // create servo object to control tilt servo
 
 void setup() {
   // initialize serial communications at 9600 bps:
   Serial.begin(9600);
-  servo1.attach(panPin);  // attaches the servo on pin 5 to the servo object
+  servoPan.attach(panPin);
+  servoTilt.attach(tiltPin);
 }
 
 void loop() {
-  Serial.println("start");
-  for (pan = 0; pan <= 60; pan += 1) { // goes from 0 degrees to 60 degrees in steps of 1 degree
-    servo1.write(pan + 60); // tell servo1 to go to position in variable 'pan'
-    //    delay(15); // waits 15ms for the servo to reach the position
-    delay(100); // waits 100ms for the sensor power to fluctuate, 40ms according to the datasheet, 100ms for good measure
-    sensorValue = analogRead(analogInPin); // read the sensnor value
-    printStatus(sensorValue, pan, 1);
+  Serial.println("start");3
+  for (pan = 80; pan <= 100; pan += 1) { // goes from 80 degrees to 110 degrees in steps of 1 degree
+    servoPan.write(pan); // tell servoPan to go to position in variable 'pan'
+    delay(20); // waits 20ms for the servo to move
+    for (tilt = -10; tilt <= 10; tilt += 1) { // for every pan degree, check every tilt degree from -20 to 20
+      servoTilt.write(-tilt+90); // tell servoTilt to go to position in variable 'tilt'
+      // +90 to account for the servo zero
+      // -tilt because the servo domain is opposite the spherical domain we want our sensor to work in
+      delay(80); // waits 80ms for the sensor power to fluctuate, 40ms according to the datasheet, 100ms for good measure and for the tilt to move
+      sensorValue = readSensor();
+      printStatus(sensorValue, pan, tilt);
+    }
   }
   Serial.println("finished");
   delay(3000);
 }
 
+int readSensor() {
+  // read the sensor and average the results
+  read1 = analogRead(analogInPin); // read the sensor value
+  delay(15);
+  read2 = analogRead(analogInPin); // read the sensor value
+  delay(15);
+  read3 = analogRead(analogInPin); // read the sensor value
+  delay(15);
+  sensorValue = (read1 + read2 + read3) / 3;
+  return sensorValue;
+}
 
 void printStatus(int sensor, int pan, int tilt) {
   // print the results to the Serial Monitor:
