45f87fafe18e007514ef9aaaecd34fc84d75685a
Griffith-Stites <gjstites@gmail.com>
2019-10-11 12:51:22 -0400

    Clean up basic following code

--- lab3/basicFollowing/basicFollowing.ino
+++ lab3/basicFollowing/basicFollowing.ino
@@ -17,7 +17,6 @@ Adafruit_DCMotor *motorRight = AFMS.getMotor(2);
 Adafruit_DCMotor *motorLeft = AFMS.getMotor(3);
 
 void setup() {
-  // put your setup code here, to run once:
   AFMS.begin();
   motorLeft->setSpeed(20);
   motorRight->setSpeed(20);
@@ -29,47 +28,18 @@ void setup() {
 void loop() {
   sensorLeft = analogRead(sensorLeftPin);
   sensorRight = analogRead(sensorRightPin);
+  // Print sensor values
   Serial.print("sensorLeft = ");
   Serial.println(sensorLeft);
   Serial.print("sensorRight = ");
   Serial.println(sensorRight);
-  // if in even farther left and off, go right
-
-  //  if (sensorLeft < 290 && sensorRight < 420) {
-  //    // if on the line
-  //  }
-
-  //  if (sensorLeft < 65 && sensorRight > 420) {
-  //    // if slightly to the left
-  //    Serial.println("RIGHT");
-  //    motorRight->setSpeed(0);
-  //    delay(300);
-  //  }
-  //  else if (sensorLeft > 470 && sensorRight < 420) {
-  //    // if slightly to the right
-  //    Serial.println("LEFT");
-  //    motorLeft->setSpeed(0);
-  //    delay(300);
-  //  }
-  //  else {
-  //    // go forward
-  //    Serial.println("FORWARD");
-  //    motorLeft->setSpeed(20);
-  //    motorRight->setSpeed(20);
-  //    motorLeft->run(BACKWARD);
-  //    motorRight->run(BACKWARD);
-  //  }
-
-  // put your main code here, to run repeatedly:
-
-  // 500
+  
   if (sensorLeft > 450) {
     // turn left
     motorLeft->setSpeed(0);
     motorRight->setSpeed(20);
     delay(300);
   }
-  // 295
   else if (sensorRight > 420) {
     // turn right
     motorLeft->setSpeed(20);
@@ -77,7 +47,7 @@ void loop() {
     delay(300);
   }
   else {
-    Serial.print("HELLO");
+    // go straight
     motorLeft->setSpeed(20);
     motorRight->setSpeed(20);
   }
